e50fda46c4a7c3413925fb7dd74c60d1a77e31d5
Sathishmahi <sathishmahi433@gmail.com>
2023-04-19 19:08:37 +0530

    to add data ingestion and basemodel config entity

---
+++ src/CNN_Classifier/entity/config_entity.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+
+DataIngestionConfig=namedtuple("DataIngestionConfig", 
+[
+    "ingestion_dir_path",
+    "video_file_path",
+    "root_image_dir_name",
+    "image_size"
+]
+)
+
+
+BaseModelConfig=namedtuple("BaseModelConfig", 
+[
+  "root_dir",
+  "base_model_dir_name",
+  "base_model_name",
+  "updated_model_dir_name",
+  "updated_model_name",
+  "image_size",
+  "no_of_classes"
+]
+)
